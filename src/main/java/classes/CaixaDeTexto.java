fcbfb39d662c3cc7c373384d77865b0bb21b3f1c
anaalthoff <anapaula.althoff@gmail.com>
2025-07-25 22:02:45 -0300

    feat: classes criadas com composição de janela com botão e caixa de texto, em que a navegabilidade ocorre de janela para os botões e caixa de texto. Portanto, a janela 'enxerga' os botões e as caixas de texto.

---
+++ src/main/java/classes/CaixaDeTexto.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class CaixaDeTexto {
+
+    private String texto;
+
+    public CaixaDeTexto() {
+        this("");
+    }
+
+    public CaixaDeTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
