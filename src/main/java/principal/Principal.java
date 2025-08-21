b5548632ec35785ce3a090b743127e4d9ed7385e
anaalthoff <anapaula.althoff@gmail.com>
2025-08-20 22:02:26 -0300

    feat: inclusão de teste de tratamento de execeções e entrada e saída de dados no Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -21,6 +21,7 @@ public class Principal {
         System.out.println("12 - Herança Tecnico e Jogador");
         System.out.println("13 - Poliformismo Herança e Sobrescrita");
         System.out.println("14 - Poliformismo Herança e Sobrescrita2");
+        System.out.println("15 - Tratamento de Execções e Entrada e Saída");
 
         int escolha = scanner.nextInt();
         scanner.nextLine();
@@ -67,6 +68,9 @@ public class Principal {
                 break;
             case 14:
                 PrincipalPoliformismoESobrescrita2.main(args);
+                break;
+            case 15:
+                PrincipalEntradaESaidaAluno.main(args);
                 break;
             default:
                 System.out.println("Opção inválida.");
